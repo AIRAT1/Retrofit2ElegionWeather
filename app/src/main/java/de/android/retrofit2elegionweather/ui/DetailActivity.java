75d25543b5d388177e877f547b36c1ca5fd6ef3f
Ayrat <ayrat1@mail.ru>
2016-08-10 23:53:11 +0300

    add DetailActivity

---
+++ app/src/main/java/de/android/retrofit2elegionweather/ui/DetailActivity.java
@@ -0,0 +1,14 @@
+package de.android.retrofit2elegionweather.ui;
+
+import android.os.Bundle;
+
+import de.android.retrofit2elegionweather.R;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
